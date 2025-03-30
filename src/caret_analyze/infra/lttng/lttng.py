e025c308c66c5fca18c2fc25bb039c7d1cafd5b6
atsushi421 <atsushi.yano.2@tier4.jp>
2025-03-31 08:48:40 +0900

    feat: add agnocast_construct_executor

--- src/caret_analyze/infra/lttng/lttng.py
+++ src/caret_analyze/infra/lttng/lttng.py
@@ -414,6 +414,7 @@ class Lttng(InfraBase):
         # For Agnocast
         'ros2:agnocast_subscription_init',
         'ros2:agnocast_publisher_init',
+        'ros2:agnocast_construct_executor',
     ]
 
     def __init__(
